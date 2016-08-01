1833585aaf73a08e88fbd2fa647a85fedc88c417
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-01 17:55:04 -0500

    importing twilio correctly

--- src/server/twilio/sms.js
+++ src/server/twilio/sms.js
@@ -1,21 +1,20 @@
-import client from 'twilio';
-client(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+import twilio from 'twilio';
+const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 const sms = {
   sendScheduledGame: ({smsNum, gameLoc, gameTime, sport}) => {
     let message = `we're playing ${sport} @ ${gameLoc} for ${gameTime}. You in?`;
     console.log(message);
-    // client.sendMessage({
-    //   to: `+1${smsNum}`,
-    //   from: `+1${process.env.TWILIO_NUM}`,
-    //   body: message
-    // }, (err, resp) => {
-    //   if (err) {
-    //     console.error('Error sending SMS: ', err);
-    //   } else {
-    //     console.log(resp);
-    //   }
-    // });
-
+    client.sendMessage({
+      to: `+1${smsNum}`,
+      from: `+1${process.env.TWILIO_NUM}`,
+      body: message
+    }, (err, resp) => {
+      if (err) {
+        console.error('Error sending SMS: ', err);
+      } else {
+        console.log(resp);
+      }
+    });
   },
 };
 
