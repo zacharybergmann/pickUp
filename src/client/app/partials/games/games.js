769c073f67f232ed3291467eed262cd4dea069be
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-02 18:24:02 -0500

    add message for enoughplayers

--- src/client/app/partials/games/games.js
+++ src/client/app/partials/games/games.js
@@ -1,10 +1,12 @@
 angular.module('games', ['pickUp.services'])
   .controller('GamesController', function ($scope, sharedProps) {
     $scope.pluralPlayers = false;
+    $scope.enoughPlayers = false;
     $scope.getGameTime = function() {
       $scope.game = {};
       $scope.game = sharedProps.get();
       $scope.game.gameTime = moment($scope.game.startTime).format('LLLL');
       $scope.pluralPlayers = $scope.game.playRequests > 1 ? true : false;
+      $scope.enoughPlayers = $scope.game.playRequests >= $scope.game.minPlayers;
     };
   });
