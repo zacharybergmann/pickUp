727fcf7b08de2ed390ae27b77e88a17317156f90
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-02 16:55:55 -0500

    fix plurality of words depending on amount of players

--- src/client/app/partials/games/games.js
+++ src/client/app/partials/games/games.js
@@ -1,8 +1,10 @@
 angular.module('games', ['pickUp.services'])
   .controller('GamesController', function ($scope, sharedProps) {
+    $scope.pluralPlayers = false;
     $scope.getGameTime = function() {
       $scope.game = {};
       $scope.game = sharedProps.get();
       $scope.game.gameTime = moment($scope.game.startTime).format('LLLL');
+      $scope.pluralPlayers = $scope.game.playRequests > 1 ? true : false;
     };
   });
