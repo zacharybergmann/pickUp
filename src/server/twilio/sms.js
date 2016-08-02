f89e28df74e4efa5ea0785d1308534f845d5f58c
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-02 12:09:29 -0500

    create Promise; add test Promise

--- src/server/twilio/sms.js
+++ src/server/twilio/sms.js
@@ -4,21 +4,30 @@ const sms = {
   sendScheduledGame: ({smsNum, gameLoc, gameTime, sport}) => {
     let message = `we're playing ${sport} @ ${gameLoc} for ${gameTime}. You in?`;
     console.log('sending message: ', message);
-    
-    // client.sendMessage({
-    //   to: `+1${smsNum}`,
-    //   from: `+1${process.env.TWILIO_NUM}`,
-    //   body: message
-    // }, (err, resp) => {
-    //   if (err) {
-    //     console.error('Error sending SMS: ', err);
-    //   } else {
-    //     console.log(resp);
-    //   }
+    // real text
+    // return new Promise((resolve, reject) => {
+    //   client.sendMessage({
+    //     to: `+1${smsNum}`,
+    //     from: `+1${process.env.TWILIO_NUM}`,
+    //     body: message
+    //   }, (err, resp) => {
+    //     if (err) {
+    //       console.error('Error sending SMS: ', err);
+    //       reject(err);
+    //     } else {
+    //       console.log(resp);
+    //       resolve(resp);
+    //     }
+    //   });
     // });
 
-
+    //test
+    return new Promise((resolve, reject) => {
+      resolve('message sent!');
+    });
   },
+
+
 };
 
 export default sms;
