9930165b9711bd4889c408190b639581e44d4fc6
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-02 19:57:29 -0500

    live texts

--- src/server/twilio/sms.js
+++ src/server/twilio/sms.js
@@ -1,30 +1,32 @@
 import twilio from 'twilio';
+import moment from 'moment';
+
 const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 const sms = {
   sendScheduledGame: ({smsNum, gameLoc, gameTime, sport}) => {
-    let message = `we're playing ${sport} @ ${gameLoc} for ${gameTime}. You in?`;
+    let message = `we're playing ${sport} @ ${gameLoc} for ${moment(gameTime).format('llll')}. You in?`;
     console.log('sending message: ', message);
     // real text
-    // return new Promise((resolve, reject) => {
-    //   client.sendMessage({
-    //     to: smsNum,
-    //     from: process.env.TWILIO_NUM,
-    //     body: message
-    //   }, (err, resp) => {
-    //     if (err) {
-    //       console.error('Error sending SMS: ', err);
-    //       reject(err);
-    //     } else {
-    //       console.log(resp);
-    //       resolve(resp);
-    //     }
-    //   });
-    // });
+    return new Promise((resolve, reject) => {
+      client.sendMessage({
+        to: smsNum,
+        from: process.env.TWILIO_NUM,
+        body: message
+      }, (err, resp) => {
+        if (err) {
+          console.error('Error sending SMS: ', err);
+          reject(err);
+        } else {
+          console.log(resp);
+          resolve(resp);
+        }
+      });
+    });
 
     //test
-    return new Promise((resolve, reject) => {
-      resolve('message sent!');
-    });
+    // return new Promise((resolve, reject) => {
+    //   resolve('message sent!');
+    // });
   },
 
 
