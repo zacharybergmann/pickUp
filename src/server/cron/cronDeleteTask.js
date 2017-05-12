9cba125749fa92a142ca7fdc0a4a236030ae57d7
charlifrank <jayfrank0528@gmail.com>
2017-05-12 12:19:37 -0500

    (bugfix) Fix error handling for cron delete

--- src/server/cron/cronDeleteTask.js
+++ src/server/cron/cronDeleteTask.js
@@ -4,18 +4,18 @@ import Game from '../games/gameModel';
 
 const cron = require('node-cron');
 
-let time = new Date();
-let checkTime = `${time.getFullYear()}-0${time.getMonth()+1}-${time.getDate()}T${time.getHours()}:00:00.000Z`;
-
 cron.schedule('*/35 * * * *', () => {
+  let time = new Date();
+  let checkTime = `${time.getFullYear()}-0${time.getMonth()+1}-${time.getDate()}T${time.getHours()}:00:00.000Z`;
   console.log(checkTime, 'checkTime in delete task file')
+  
   Game.find({ 'startTime': checkTime }, (err, games) => {
     if (err) {
       console.error(err, 'Err');
+    } else if (games.length === 0) {
+      return; 
     } else {
-      console.log(games[0]._id, 'Games');
       games.forEach(game => {
-        console.log(game, 'game in loop')
         Game.remove({ _id: game._id }, function(error, data) {
           if (error) {
              console.error(error, 'Error');
