608a88e73bd7c4e6a5294ecd77dd9890e9696bcf
charlifrank <jayfrank0528@gmail.com>
2017-05-15 15:22:53 -0500

    (bugfix) Send messages to all games users for each game 30 minutes before game

--- src/server/cron/cronMessageTask.js
+++ src/server/cron/cronMessageTask.js
@@ -19,37 +19,40 @@ cron.schedule('30 * * * *', () => {
     } else if (games.length === 0) {
       return;
     } else {
-      if (games[0].minPlayers <= games[0].playRequests) {
-        games[0].smsNums.forEach((num, index) => {
-          if (index === 0) { num.smsNum = num.smsNum.slice(2); }
-          client.sendMessage({
-              to: `+1${num.smsNum}`,
-              from: process.env.TWILIO_NUM,
-              body: `Hey, don't forget about the ${games[0].sport} game at ${games[0].startTime.getHours() >= 12 ? games[0].startTime.getHours() - 12: games[0].startTime.getHours()}:00! Have fun! :)`
-            }, (err, resp) => {
-              if (err) {
-                console.error('Error sending SMS: ', err);
-              } else {
-                console.log(resp, 'response');
-              }
-            });
-          })
-        } else {
-          games[0].smsNums.forEach((num, index) => {
+      games.forEach((game, index) => {
+        if (game.minPlayers <= game.playRequests) {
+          game.smsNums.forEach((num, index) => {
+          console.log(num, 'num in for each in if')
             if (index === 0) { num.smsNum = num.smsNum.slice(2); }
             client.sendMessage({
-              to: `+1${num.smsNum}`,
-              from: process.env.TWILIO_NUM,
-              body: `Hey, sorry the ${games[0].sport} game you wanted to play at ${games[0].startTime.getHours() >= 12 ? games[0].startTime.getHours() - 12: games[0].startTime.getHours()}:00 didn't get enough players. Feel free to try back another time.`
-            }, (err, resp) => {
-              if (err) {
-                console.error('Error sending SMS: ', err);
-              } else {
-                console.log(resp, 'response');
-              }
-            });
-          })
-        }
+                to: `+1${num.smsNum}`,
+                from: process.env.TWILIO_NUM,
+                body: `Hey, don't forget about the ${games[0].sport} game at ${games[0].startTime.getHours() >= 12 ? games[0].startTime.getHours() - 12: games[0].startTime.getHours()}:00! Have fun! :)`
+              }, (err, resp) => {
+                if (err) {
+                  console.error('Error sending SMS: ', err);
+                } else {
+                  console.log(resp, 'response');
+                }
+              });
+            })
+          } else {
+            game.smsNums.forEach((num, index) => {
+              if (index === 0) { num.smsNum = num.smsNum.slice(2); }
+              client.sendMessage({
+                to: `+1${num.smsNum}`,
+                from: process.env.TWILIO_NUM,
+                body: `Hey, sorry the ${games[0].sport} game you wanted to play at ${games[0].startTime.getHours() >= 12 ? games[0].startTime.getHours() - 12: games[0].startTime.getHours()}:00 didn't get enough players. Feel free to try back another time.`
+              }, (err, resp) => {
+                if (err) {
+                  console.error('Error sending SMS: ', err);
+                } else {
+                  console.log(resp, 'response');
+                }
+              });
+            })
+          }
+      })
     }
   });
 })
